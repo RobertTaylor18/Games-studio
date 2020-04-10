80d791bdc3f9d1fd0a01bb1f508be7ef292eb7e7
RobertTaylor <56117726+RobertTaylor18@users.noreply.github.com>
2020-04-10 15:09:22 +0100

    Added character swap on mouse click

--- Games studio/Assets/CharSwap.cs
+++ Games studio/Assets/CharSwap.cs
@@ -7,14 +7,38 @@ public class CharSwap : MonoBehaviour
 
     public GameObject char1;
     public GameObject char2;
+    
+    public CharacterController char1control;
+    //public FirstPersonController char1script;
+    public Camera char1cam;
+    
+    public CharacterController char2control;
+   // public FirstPersonController char2script;
+    public Camera char2cam;
+
     public int character = 1;
     public float swapTimer = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        char1.gameObject.SetActive(true);
-        char2.gameObject.SetActive(false);
+        // char1.gameObject.SetActive(true);
+        // char2.gameObject.SetActive(false);
+
+        char1control = char1.GetComponent<CharacterController>();
+       // char1script = char1.GetComponent<FirstPersonController>();
+        char1cam = char1.GetComponentInChildren<Camera>();
+        
+        char2control = char2.GetComponent<CharacterController>();
+      //  char2script = char2.GetComponent<FirstPersonController>();
+        char2cam = char2.GetComponentInChildren<Camera>();
+        
+       // char1.gameObject.SetActive(true);
+        //char2.gameObject.SetActive(false);
+
+        char2control.enabled = false;
+        char2cam.enabled = false;
+        
     }
 
     // Update is called once per frame
@@ -30,16 +54,22 @@ public class CharSwap : MonoBehaviour
 
         if (Input.GetButton("Fire1") && character == 1 && swapTimer == 0)
         {
-            char1.gameObject.SetActive(false);
-            char2.gameObject.SetActive(true);
+            char1control.enabled = false;
+            char1cam.enabled = false;
+            
+            char2control.enabled = true;
+            char2cam.enabled = true;
 
             character = 2;
             swapTimer = 2;
         }
         else if (Input.GetButton("Fire1") && character == 2 && swapTimer == 0)
         {
-            char1.gameObject.SetActive(true);
-            char2.gameObject.SetActive(false);
+            char1control.enabled = true;
+            char1cam.enabled = true;
+
+            char2control.enabled = false;
+            char2cam.enabled = false;
 
             character = 1;
             swapTimer = 2;
