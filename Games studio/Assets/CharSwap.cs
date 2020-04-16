29a6806a8e3d26f7a3da775277b1e519280b0f89
RobertTaylor <56117726+RobertTaylor18@users.noreply.github.com>
2020-04-16 19:54:35 +0100

    Fixing incorrect mouselook on other character

--- Games studio/Assets/CharSwap.cs
+++ Games studio/Assets/CharSwap.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets;
 
 public class CharSwap : MonoBehaviour
 {
@@ -9,11 +10,11 @@ public class CharSwap : MonoBehaviour
     public GameObject char2;
     
     public CharacterController char1control;
-    //public FirstPersonController char1script;
+    public FirstPersonController firstPersonController;
     public Camera char1cam;
     
     public CharacterController char2control;
-   // public FirstPersonController char2script;
+    //public FirstPersonController char2script;
     public Camera char2cam;
 
     public int character = 1;
@@ -26,11 +27,11 @@ public class CharSwap : MonoBehaviour
         // char2.gameObject.SetActive(false);
 
         char1control = char1.GetComponent<CharacterController>();
-       // char1script = char1.GetComponent<FirstPersonController>();
+        firstPersonController = char1.GetComponent<FirstPersonController>();
         char1cam = char1.GetComponentInChildren<Camera>();
         
         char2control = char2.GetComponent<CharacterController>();
-      //  char2script = char2.GetComponent<FirstPersonController>();
+        //char2script = char2.GetComponent<FirstPersonController>();
         char2cam = char2.GetComponentInChildren<Camera>();
         
        // char1.gameObject.SetActive(true);
@@ -38,7 +39,10 @@ public class CharSwap : MonoBehaviour
 
         char2control.enabled = false;
         char2cam.enabled = false;
-        
+       // char2script.enabled = false;
+
+       // char2.GetComponent<FirstPersonController>().enabled = false;
+
     }
 
     // Update is called once per frame
@@ -56,9 +60,14 @@ public class CharSwap : MonoBehaviour
         {
             char1control.enabled = false;
             char1cam.enabled = false;
+            char1script.enabled = false;
             
             char2control.enabled = true;
             char2cam.enabled = true;
+            char2script.enabled = true;
+            
+            //char1.GetComponent<FirstPersonController>().enabled = false;
+           // char2.GetComponent<FirstPersonController>().enabled = true;
 
             character = 2;
             swapTimer = 2;
@@ -70,6 +79,9 @@ public class CharSwap : MonoBehaviour
 
             char2control.enabled = false;
             char2cam.enabled = false;
+
+           // char1.GetComponent<FirstPersonController>().enabled = true;
+            //char2.GetComponent<FirstPersonController>().enabled = false;
 
             character = 1;
             swapTimer = 2;
