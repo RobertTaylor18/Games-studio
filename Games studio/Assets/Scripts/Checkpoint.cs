0898879f97bb2afc18b4f3b7a14ce008a423f7df
RobertTaylor <56117726+RobertTaylor18@users.noreply.github.com>
2020-05-22 14:54:37 +0100

    Added Room4 mechanics, Killzone and checkpoints

---
+++ Games studio/Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    public KillZone killScript;
+    public BoxCollider BoxCol;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        killScript = GameObject.Find("KillZone").GetComponent<KillZone>();
+        BoxCol = GetComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter()
+    {
+        killScript.checkpoint++;
+        BoxCol.enabled = false;
+        
+    }
+}
---
+++ Games studio/Assets/Scripts/GirderSwing.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GirderSwing : MonoBehaviour
+{
+
+    public float swingTimer;
+    public float swingTimerSin;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Swing();
+    }
+
+    void Swing()
+    {
+        swingTimer += Time.deltaTime;
+
+        swingTimerSin = Mathf.Sin(swingTimer) * 10;
+
+        this.transform.position = new Vector3(57f+swingTimerSin, 9f, -38f);
+    }
+}
