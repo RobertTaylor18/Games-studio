9c17022aa5eca65c4c8d547ddc73217609eaaf71
RobertTaylor18 <56117726+RobertTaylor18@users.noreply.github.com>
2020-05-22 18:23:54 +0100

    Revert "Danny"

--- Games studio/Assets/Scripts/PlayerScripts/CharacterController1.cs
+++ Games studio/Assets/Scripts/PlayerScripts/CharacterController1.cs
@@ -8,9 +8,6 @@ public class CharacterController1 : MonoBehaviour
     public float inputDelay, forwardVel, jumpForce, fallMultiplier;
     float forwardInput, sideInput;
     public float grav, fallgrav;
-
-    public Vector3 Mag;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -20,17 +17,15 @@ public class CharacterController1 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Mag = rBody.velocity;
         GetInput();
 
         grav = Physics.gravity.y;
 
         if (rBody.velocity.y < 0)
         {
-
             //rBody.velocity += new Vector3 (0f,-9.81f * fallMultiplier,0f);
             // rBody.AddForce(0f,-9.81f,0f);
-            //rBody.AddForce(Vector3.up * Physics.gravity.y * fallMultiplier);
+             rBody.AddForce(Vector3.up * Physics.gravity.y * fallMultiplier);
         }
         fallgrav = rBody.velocity.y;
     }
@@ -52,9 +47,7 @@ public class CharacterController1 : MonoBehaviour
         
         if (Mathf.Abs(forwardInput) > inputDelay || Mathf.Abs(sideInput) > inputDelay)
         {
-            Vector3 temp = (transform.forward * forwardInput * forwardVel) + (transform.right * sideInput * forwardVel);
-            rBody.velocity = new Vector3(temp.x, rBody.velocity.y, temp.z);
-            //rBody.velocity = (transform.forward * forwardInput * forwardVel) + (transform.right * sideInput * forwardVel);
+            rBody.velocity = (transform.forward * forwardInput * forwardVel) + (transform.right * sideInput * forwardVel);
 
             //Sprint
             /*if (Input.GetButtonDown("Fire3"))
@@ -64,16 +57,13 @@ public class CharacterController1 : MonoBehaviour
 
             if (Input.GetButtonDown("Fire3"))
             {
-                rBody.velocity = new Vector3(sideInput*100, rBody.velocity.y, forwardInput * 100); 
+                rBody.velocity = new Vector3(sideInput*100, 0f, forwardInput * 100);
             }
            
         }
         else
         {
-            //rBody.velocity = Vector3.zero;
-            rBody.velocity = new Vector3(0, rBody.velocity.y, 0);
-
-
+            rBody.velocity = Vector3.zero;
             rBody.position = rBody.position;
         }
 
@@ -88,8 +78,8 @@ public class CharacterController1 : MonoBehaviour
     {
         if (Input.GetButtonDown("Jump"))
         {
-            rBody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            //rBody.velocity = new Vector3(0f,jumpForce,0f);
+           rBody.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+           //rBody.velocity = new Vector3(0f,jumpForce,0f);
         }
     }
 }
